117045d79ef2b460e8c7a5da8b97c3e9f2f7dbf1
SohamDas00 <jisusoham04@gmail.com>
2025-04-08 00:42:40 +0530

    fix logo

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-        <img src="/src/assets/troll-face.png" alt="troll-face" className='pic'/>
+        <img src="troll-face.png" alt="troll-face" className='pic'/>
         <h2 className='heading'>Meme Generator</h2>
     </div>
   )
